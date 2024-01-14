ed0c90fc2ce827e36d9880cacf8704593dd566c3
Milton <miltonio94@gmail.com>
2024-01-14 16:42:05 +0000

    Task Dbus exploration

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,89 @@
-extern crate daemonize;
-
-use daemonize::Daemonize;
-use notify_rust::Notification;
-use signal_hook::iterator::SignalsInfo;
-use signal_hook::{consts::SIGTERM, iterator::Signals};
-use std::fs;
-use std::io::{self, Write};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 use std::time::Duration;
-use std::{fs::File, thread};
-// TODO: Figure out how to delete tmp file once program exists #2
 
+use dbus::blocking::Connection;
+use dbus::channel::MatchingReceiver;
+use dbus::message::MatchRule;
+use dbus::Message;
+
+// This programs implements the equivalent of running the "dbus-monitor" tool
 fn main() {
-    println!("starting daemon for green shark");
+    // Very simple argument parsing.
 
-    let stdout = File::create("/tmp/green-shark.out").unwrap();
-    let stderr = File::create("/tmp/green-shark.err").unwrap();
+    // First open up a connection to the desired bus.
+    let conn = Connection::new_system().expect("D-Bus connection failed");
 
-    let pid_file = Arc::new("/tmp/shark.pid");
-    let pid_file_ref = pid_file.clone();
+    // Second create a rule to match messages we want to receive; in this example we add no
+    // further requirements, so all messages will match
+    let rule = MatchRule::new();
 
-    let sleep_time = Duration::new(8, 0);
+    // Try matching using new scheme
+    let proxy = conn.with_proxy(
+        "org.freedesktop.DBus",
+        "/org/freedesktop/DBus",
+        Duration::from_millis(5000),
+    );
+    let result: Result<(), dbus::Error> = proxy.method_call(
+        "org.freedesktop.DBus.Monitoring",
+        "BecomeMonitor",
+        (vec![rule.match_str()], 0u32),
+    );
 
-    let daemonize = Daemonize::new()
-        .pid_file(*pid_file)
-        .chown_pid_file(true)
-        .working_directory("/tmp")
-        .user("jack")
-        .group("jack") // Group name
-        .group(2) // or group id.
-        .umask(0o1777) // Set umask, `0o027` by default.
-        .stdout(stdout)
-        .stderr(stderr)
-        .privileged_action(|| "Executed before drop privileges");
-
-    match daemonize.start() {
-        Ok(r) => {
-            println!("Success, daemonized {}", r);
-            io::stdout().flush().unwrap();
-            return;
+    match result {
+        // BecomeMonitor was successful, start listening for messages
+        Ok(_) => {
+            conn.start_receive(
+                rule,
+                Box::new(|msg, _| {
+                    handle_message(&msg);
+                    true
+                }),
+            );
         }
+        // BecomeMonitor failed, fallback to using the old scheme
+        Err(e) => {
+            eprintln!(
+                "Failed to BecomeMonitor: '{}', falling back to eavesdrop",
+                e
+            );
 
-        Err(e) => eprintln!("This Error, {}", e),
-    }
+            // First, we'll try "eavesdrop", which as the name implies lets us receive
+            // *all* messages, not just ours.
+            let rule_with_eavesdrop = {
+                let mut rule = rule.clone();
+                rule.eavesdrop = true;
+                rule
+            };
 
-    let signal_handler_thread = thread::spawn(move || {
-        let mut signal: SignalsInfo = Signals::new(&[SIGTERM]).expect("Bloody thing broked.");
-        for sig in signal.forever() {
-            match sig {
-                SIGTERM => {
-                    if let Err(e) = fs::remove_file(*pid_file_ref) {
-                        eprintln!("Unable to remove file. {:?}", e)
-                    }
+            let result = conn.add_match(rule_with_eavesdrop, |_: (), _, msg| {
+                handle_message(&msg);
+                true
+            });
+
+            match result {
+                Ok(_) => {
+                    // success, we're now listening
                 }
-                _ => {}
+                // This can sometimes fail, for example when listening to the system bus as a non-root user.
+                // So, just like `dbus-monitor`, we attempt to fallback without `eavesdrop=true`:
+                Err(e) => {
+                    eprintln!("Failed to eavesdrop: '{}', trying without it", e);
+                    conn.add_match(rule, |_: (), _, msg| {
+                        handle_message(&msg);
+                        true
+                    })
+                    .expect("add_match failed");
+                }
             }
         }
-    });
-
-    loop {
-        thread::sleep(sleep_time);
-        Notification::new()
-            .summary("hello green shark")
-            .body("Green Shark running")
-            .show()
-            .unwrap();
     }
 
-    signal_handler_thread.join().expect("Thread panicked!");
+    // Loop and print out all messages received (using handle_message()) as they come.
+    // Some can be quite large, e.g. if they contain embedded images..
+    loop {
+        conn.process(Duration::from_millis(1000)).unwrap();
+    }
+}
+
+fn handle_message(msg: &Message) {
+    println!("Got message: {:?}", msg);
 }
