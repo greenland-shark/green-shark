03a14e64df1ad6d43322766709764121602741ce
Milton De Paula <8180004+miltonio94@users.noreply.github.com>
2024-03-05 20:55:17 +0000

    GS-6 Task complete one off transaction dbus interface (#7)
    * GS-6 Task complete one off transaction dbus interface
    
    also separated files to make testing of library easier
    
    * Test transaction uids are unique
    
    * Fix Ensure uids are unique
    
    now in the case of transactions made together may be the same.
    As such I am suggesting to add a random number to make a uid more unique.
    Something between 1 and 100 seems okay.
    
    * GS-6 Refactor Tests into folder
    
    ---------
    
    Co-authored-by: Jack <jackmurphy756@gmail.com>

---
+++ src/lib/transaction.rs
@@ -0,0 +1,70 @@
+use chrono::Utc;
+use rand::Rng;
+
+#[derive(Debug)]
+pub enum Currency {
+    NZD,
+    GBP,
+    BRL,
+}
+
+#[derive(Debug)]
+pub enum Frequency {
+    OneOff(i64),
+    // Monthly(u8),
+    // Weekly(u8),
+    // Yearly((u8, u8)),
+}
+
+impl Frequency {
+    pub fn one_off() -> Self {
+        let now = Utc::now();
+        let now = now.timestamp();
+        Self::OneOff(now)
+    }
+}
+
+type Amount = (Currency, f32);
+
+#[derive(Debug)]
+pub struct Transaction {
+    pub amount: Amount,
+    pub name: String,
+    pub label: Option<String>,
+    id: i64,
+    pub frequency: Frequency,
+    pub start_date: i64,
+    pub end_date: Option<i64>,
+}
+
+impl Transaction {
+    pub fn new(
+        amount: Amount,
+        name: String,
+        label: Option<String>,
+        frequency: Frequency,
+        end_date: Option<i64>,
+    ) -> Self {
+        let now = Utc::now();
+        let mut rng = rand::thread_rng();
+        let n: i64 = rng.gen_range(0..100);
+        let uid = now.timestamp() + n;
+        let now = now.timestamp();
+        Self {
+            amount,
+            name,
+            label,
+            id: uid,
+            frequency,
+            start_date: now,
+            end_date,
+        }
+    }
+
+    pub fn id(&self) -> i64 {
+        self.id
+    }
+    pub fn start_date(&self) -> i64 {
+        self.start_date
+    }
+}
