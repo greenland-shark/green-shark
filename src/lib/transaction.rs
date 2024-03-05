f66415b3ccae043c5064ae2ae9f222cd48fe4321
Jack <jackmurphy756@gmail.com>
2024-03-05 20:38:12 +0000

    GS-6 Refactor Tests into folder

--- src/lib/transaction.rs
+++ src/lib/transaction.rs
@@ -68,36 +68,3 @@ impl Transaction {
         self.start_date
     }
 }
-
-#[cfg(test)]
-mod test {
-    use crate::transaction::{Currency, Frequency, Transaction};
-
-    #[test]
-    fn uids_are_unique() {
-        let transaction_1 = Transaction::new(
-            (Currency::GBP, 10.5),
-            "out1".to_string(),
-            Some("drink".to_string()),
-            Frequency::one_off(),
-            None,
-        );
-        let transaction_2 = Transaction::new(
-            (Currency::GBP, 10.5),
-            "out1".to_string(),
-            Some("drink".to_string()),
-            Frequency::one_off(),
-            None,
-        );
-
-        assert_ne!(
-            transaction_1.id(), transaction_1.start_date(),
-            "UID shouldn't match start_date because start_date can be the same for multiple transactions."
-        );
-        assert_ne!(
-            transaction_1.id(),
-            transaction_2.id(),
-            "UIDs shouldn't match where transactions may be created at (or near) the same time."
-        );
-    }
-}
---
+++ tests/transaction.rs
@@ -0,0 +1,29 @@
+use green_shark::transaction::{Currency, Frequency, Transaction};
+
+#[test]
+fn uids_are_unique() {
+    let transaction_1 = Transaction::new(
+        (Currency::GBP, 10.5),
+        "out1".to_string(),
+        Some("drink".to_string()),
+        Frequency::one_off(),
+        None,
+    );
+    let transaction_2 = Transaction::new(
+        (Currency::GBP, 10.5),
+        "out1".to_string(),
+        Some("drink".to_string()),
+        Frequency::one_off(),
+        None,
+    );
+
+    assert_ne!(
+        transaction_1.id(), transaction_1.start_date(),
+        "UID shouldn't match start_date because start_date can be the same for multiple transactions."
+        );
+    assert_ne!(
+        transaction_1.id(),
+        transaction_2.id(),
+        "UIDs shouldn't match where transactions may be created at (or near) the same time."
+    );
+}
