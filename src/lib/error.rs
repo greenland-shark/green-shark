fc6190663621e8ab57e875e9ff17ac84aa5ba7b7
Jack <jackmurphy756@gmail.com>
2024-03-06 18:39:06 +0000

    GS-13 Test state can be written/read to file

---
+++ src/lib/error.rs
@@ -0,0 +1,13 @@
+use std::fmt::Debug;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Failed to parse json.")]
+    SerdeJsonError(#[from] serde_json::Error),
+
+    #[error("failed to read file.")]
+    FileReadError(#[source] std::io::Error),
+
+    #[error("failed to write file.")]
+    FileWriteError(#[source] std::io::Error),
+}
--- src/lib/state.rs
+++ src/lib/state.rs
@@ -1,16 +1,54 @@
+use crate::{
+    error::Error,
+    transaction::{Currency, Frequency, Transaction},
+};
+use serde::{Deserialize, Serialize};
 use zbus::dbus_interface;
-use crate::transaction::{Transaction, Currency, Frequency};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct State {
-    pub transactions: Vec<Transaction>,
+    pub incomes: Vec<Transaction>,
+    pub outgoings: Vec<Transaction>,
 }
 
 impl State {
     pub fn new() -> State {
         State {
-            transactions: vec![],
+            incomes: vec![],
+            outgoings: vec![],
         }
+    }
+
+    pub fn to_file(&self, filepath: &str) -> Result<(), Error> {
+        let json_string = serde_json::to_string(&self).map_err(Error::SerdeJsonError)?;
+        std::fs::write(filepath, json_string).map_err(Error::FileWriteError)
+    }
+
+    /// Where a state file does not exist, `from_file` creates a file and an empty state.  
+    pub fn from_file(f: &str) -> Result<Self, Error> {
+        let filepath = f;
+        let file_string = std::fs::read_to_string(filepath).map_err(Error::FileReadError);
+        match file_string {
+            Ok(s) => {
+                // read state from file
+                serde_json::from_str(&*s).map_err(Error::SerdeJsonError)
+            }
+            Err(_) => {
+                let new_state = State::new();
+                State::to_file(&new_state, filepath)?;
+                Ok(new_state)
+            }
+        }
+    }
+
+    pub fn add_income(&mut self, t: Transaction) -> &mut Self {
+        self.incomes.push(t);
+        self
+    }
+
+    pub fn add_outgoing(&mut self, t: Transaction) -> &mut Self {
+        self.outgoings.push(t);
+        self
     }
 }
 
@@ -33,13 +71,13 @@ impl State {
             frequency,
             end_date,
         );
-        self.transactions.push(transaction);
+        self.outgoings.push(transaction);
 
         format!("Transactions: {:?}", self)
     }
 
     #[dbus_interface(property)]
-    async fn transactions(&self) -> String {
-        format!("{:?}", self.transactions)
+    async fn outgoings(&self) -> String {
+        format!("{:?}", self.outgoings)
     }
 }
