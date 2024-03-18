7f7edec866f28587853a20d30ff0a8fdd8111693
Milton <miltonio94@gmail.com>
2024-03-18 16:17:43 +0000

    GS-16 Feature implement clap interface for cli

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,5 +1,49 @@
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
 fn main() {
+    let args = Cli::parse();
     println!("Hello, world!");
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    subject: Subject,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+enum Subject {
+    // Doc comment
+    Income(Action),
+    // Doc comment
+    Outcome(Action),
+}
+
+#[derive(Clone, Debug, Args)]
+struct Action {
+    #[command(subcommand)]
+    action: Action_,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum Action_ {
+    Add(Flags),
+    Get(Flags),
+}
+
+#[derive(Clone, Debug, Args)]
+struct Flags {
+    #[arg[long, value_name = "AMOUNT"]]
+    amount: i32,
+    #[arg[long, value_name = "NAME"]]
+    name: String,
+    #[arg[long, value_name = "LABEL"]]
+    label: String,
+    #[arg[long, value_name = "START DATE"]]
+    start_date: Option<String>,
+    #[arg[long, value_name = "END DATE"]]
+    end_date: Option<String>,
 }
 
 // shark [SUBJECT] [Optional [ACTION]] (flags)
