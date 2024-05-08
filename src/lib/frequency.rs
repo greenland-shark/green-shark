b95bcbcb4d721e9ae0246726d7a17db903f94a9c
Jack <jackmurphy756@gmail.com>
2024-05-08 23:40:09 +0100

    GS-8 Refactor to rely more on NaiveDate

--- src/lib/frequency.rs
+++ src/lib/frequency.rs
@@ -1,48 +1,39 @@
-use chrono::{Days, Months, NaiveDate, NaiveDateTime, NaiveTime, Utc};
-use either::Either;
+use crate::error::Error;
+use chrono::{Datelike, Days, Duration, NaiveDate, NaiveDateTime, NaiveTime, Utc, Weekday};
 use serde::{Deserialize, Serialize};
 
-type UnixT = i64;
-pub type NumberMonths = u32;
-pub type NumberWeeks = u64;
+// TODO Performance testing on the bus
 
-/// Useful in CLI where providing and end after n...
-pub fn num_occurences<T>(num: T) -> EndOf<T> {
-    Either::Right(num)
-}
+// TODO Add this logic to view
+// If Freq start is not after window end and no end to Transaction Freq, then find and show transaction
+// Or if Freq start is not after window end and end of Frequency end IS after view end, then find and show transaction
+
+type UnixT = i64;
+pub type Year = i32;
+pub type Month = u32;
+
+pub type Weeks = i64;
+pub type Months = u32;
 
 /// Corresponds with args to NaiveDate from_ymd
 /// When not in a struct(or in variables), the values could easily be mixed up
-pub struct StartDate {
-    pub year: i32,
-    pub month: NumberMonths,
+#[derive(Debug, PartialEq, Clone, Copy, Deserialize, Serialize)]
+pub struct Date {
+    pub year: Year,
+    pub month: Month,
     pub date: u32,
-}
-
-pub struct EndDate {
-    year: i32,
-    month: NumberMonths,
-    date: u32,
-}
-
-type EndOf<T> = Either<EndDate, T>;
-
-impl EndDate {
-    pub fn new<T>(year: i32, month: NumberMonths, date: u32) -> EndOf<T> {
-        Either::Left(Self { year, month, date })
-    }
 }
 
 #[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
 /// TODO Yearly(u8, u8),
 pub enum Frequency {
-    OneOff(i64),
+    OneOff(UnixT),
 
-    /// Monthly transaction start and possible finish
-    MonthlyByDate(UnixT, Option<UnixT>),
+    /// Weekly transaction start and possible end date
+    Weekly(NaiveDate, Option<NaiveDate>, Weekday),
 
-    /// Weekly transaction start and possible finish
-    Weekly(UnixT, Option<UnixT>),
+    /// Monthly transaction start and possible end date
+    MonthlyByDate(NaiveDate, Option<NaiveDate>),
     // Yearly(u8, u8),
 }
 
@@ -53,50 +44,65 @@ impl Frequency {
         Self::OneOff(now)
     }
 
-    /// Define a transaction to be on a fixed date each month (Default start is today).
-    /// **TODO** Where the date doesn't exist in the month, CLI should retreive the closest earlier date.
-    /// **TODO** Handle/remove unwraps
-    /// **TODO** Think the CLI should have some logic along the lines - if value from previous
-    /// view == Monthly then check if a repeat occurence is needed in current view
-    pub fn monthly_by_date(start: StartDate, end: Option<EndOf<NumberMonths>>) -> Self {
-        let dt = NaiveDate::from_ymd_opt(start.year, start.month, start.date).unwrap();
-        let t = NaiveTime::from_hms_milli_opt(0, 0, 0, 0).unwrap();
-        let start_date = NaiveDateTime::new(dt, t).and_utc().timestamp();
+    /// Define a transaction to be on a fixed day each week.
+    pub fn weekly(start: Date, num_weeks: Option<Weeks>) -> Option<Self> {
+        let naive_end = num_weeks.and_then(|n| increase_date_n_weeks(start.to_owned(), n));
 
-        let end_date = end.map(|end| match end {
-            Either::Left(e) => {
-                let the_end = NaiveDate::from_ymd_opt(e.year, e.month, e.date).unwrap();
-                NaiveDateTime::new(the_end, t).and_utc().timestamp()
-            }
-            Either::Right(occurences) => {
-                let months = Months::new(occurences);
-                let from_start = dt.checked_add_months(months).unwrap();
-                NaiveDateTime::new(from_start, t).and_utc().timestamp()
-            }
-        });
-
-        Self::MonthlyByDate(start_date, end_date)
+        from_date(start).map(|naive_start| {
+            let weekday = naive_start.weekday();
+            Self::Weekly(naive_start, naive_end, weekday)
+        })
     }
 
-    /// Define a transaction to be on a fixed day each week (Default start is today).
-    /// TODO Find out which day is current
-    pub fn weekly(start: StartDate, end: Option<EndOf<NumberWeeks>>) -> Self {
-        let dt = NaiveDate::from_ymd_opt(start.year, start.month, start.date).unwrap();
-        let t = NaiveTime::from_hms_milli_opt(0, 0, 0, 0).unwrap();
-        let start_date = NaiveDateTime::new(dt, t).and_utc().timestamp();
+    /// Define a transaction to be on a fixed date each month.
+    /// Where date doesn't exist in month, the closest suitable date should be used.
+    pub fn monthly_by_date(start: Date, num_months: Option<Months>) -> Option<Self> {
+        let naive_end = num_months.and_then(|n| increase_date_n_months(start.to_owned(), n));
+        from_date(start).map(|naive_start| Self::MonthlyByDate(naive_start, naive_end))
+    }
+}
 
-        let end_date = end.map(|end| match end {
-            Either::Left(e) => {
-                let the_end = NaiveDate::from_ymd_opt(e.year, e.month, e.date).unwrap();
-                NaiveDateTime::new(the_end, t).and_utc().timestamp()
-            }
-            Either::Right(number_weeks) => {
-                let weeks = Days::new(number_weeks * 7);
-                let from_start = dt.checked_add_days(weeks).unwrap();
-                NaiveDateTime::new(from_start, t).and_utc().timestamp()
-            }
-        });
+pub fn from_date(d: Date) -> Option<NaiveDate> {
+    NaiveDate::from_ymd_opt(d.year, d.month, d.date)
+}
 
-        Self::Weekly(start_date, end_date)
+pub fn increase_date_n_weeks(start: Date, num_weeks: Weeks) -> Option<NaiveDate> {
+    let n = Duration::weeks(num_weeks);
+    from_date(start).map(|date| date + n)
+}
+
+pub fn increase_date_n_months(start: Date, num_months: Months) -> Option<NaiveDate> {
+    from_date(start).and_then(|date| add_months(date, num_months))
+}
+
+fn add_months(date: NaiveDate, num_months: u32) -> Option<NaiveDate> {
+    let mut month = date.month() + num_months;
+    let year = date.year() + (month / 12) as i32;
+    month = month % 12;
+
+    let mut day = date.day();
+    get_days_from_month(year, month).and_then(|max_days| {
+        day = if day > max_days { max_days } else { day };
+        NaiveDate::from_ymd_opt(year, month, day)
+    })
+}
+
+fn get_days_from_month(year: Year, month: Month) -> Option<u32> {
+    let start_next_month = NaiveDate::from_ymd_opt(
+        match month {
+            12 => year + 1,
+            _ => year,
+        },
+        match month {
+            12 => 1,
+            _ => month + 1,
+        },
+        1,
+    );
+    let start_curr = NaiveDate::from_ymd_opt(year, month, 1);
+
+    match (start_next_month, start_curr) {
+        (Some(nxt), Some(cur)) => Some(nxt.signed_duration_since(cur).num_days() as u32),
+        _ => None,
     }
 }
